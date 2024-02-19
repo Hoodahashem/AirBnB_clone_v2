fa03e511a1ed6b9ffd73d9733a45bd198ea26663
Hoodahashem <mahmoudhashem335@gmail.com>
2024-02-19 21:01:54 +0200

    fixing bug!

--- models/review.py
+++ models/review.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from os import getenv
+import models
 
 
-class Review(BaseModel):
-    """ Review classto store review information """
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
+class Review(BaseModel, Base):
+    """Representation of Review """
+    if models.storage_t == 'db':
         __tablename__ = 'reviews'
         place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
