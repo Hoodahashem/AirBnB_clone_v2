ed327e18bc6569df2e3f0ce972b1d851040972c9
Hoodahashem <mahmoudhashem335@gmail.com>
2024-02-21 01:18:07 +0200

    5th script!

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" display “Hello HBNB!” """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """C"""
+    return ('C {}'.format(text.replace('_', ' ')))
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text):
+    """Python"""
+    return ('Python {}'.format(text.replace('_', ' ')))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """Number"""
+    return ('{} is a number'.format(n))
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
