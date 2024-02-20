11ea032078b1ef2e8515ac3d6f28f06fb93cbb18
Hoodahashem <mahmoudhashem335@gmail.com>
2024-02-21 01:16:02 +0200

    4th script!

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -17,5 +17,17 @@ def hbnb():
     return 'HBNB'
 
 
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """C"""
+    return ('C {}'.format(text.replace('_', ' ')))
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text):
+    """Python"""
+    return ('Python {}'.format(text.replace('_', ' ')))
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
