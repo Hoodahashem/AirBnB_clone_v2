2be3cc04dc7d1a9e3a44f53d6541e3b1eb7873ca
Hoodahashem <mahmoudhashem335@gmail.com>
2024-02-21 17:18:33 +0200

    7th script!

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Start a Flask web application"""
+from flask import Flask, render_template
+from models import storage
+from models import *
+
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Display an HTML page with a list of all the states"""
+    states = storage.all('State').values()
+    return render_template('7-states_list.html', states=states)
+
+
+
+@app.teardown_appcontext
+def close_session(exception):
+    """Close the current SQLAlchemy session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
